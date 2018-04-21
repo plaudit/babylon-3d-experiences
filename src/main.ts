708a0e538211ff1a8e9b53c88fcb872527bc4d53
David Schlotfeldt <david@plaudit.com>
2018-04-21 16:39:05 -0500

    Add logic to decide when to render, refactored alot

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,14 @@
-import * as BABYLON from 'babylonjs';
+import CookieCutterSuburbia from './CookieCutterSuburbia';
+import SmartRender from './SmartRender';
 
-class Main {
-	private engine: BABYLON.Engine;
-	private scene: BABYLON.Scene;
-	private canvas: HTMLCanvasElement;
-
-	constructor(canvas: HTMLCanvasElement) {
-		this.canvas = canvas;
-
-		const engine = this.engine = new BABYLON.Engine(
-			this.canvas,
-			true,	// antialias
-			{},		// options
-			true	// adaptToDeviceRatio
-		);
-
-		const scene = this.scene = new BABYLON.Scene(this.engine);
-
-		// Add a camera to the scene and attach it to the canvas
-		const camera = new BABYLON.ArcRotateCamera('Camera', Math.PI / 2, Math.PI / 2, 2, BABYLON.Vector3.Zero(), scene);
-		camera.attachControl(canvas, true);
-
-		// Add lights to the scene
-		const light1 = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(1, 1, 0), scene);
-		const light2 = new BABYLON.PointLight('light2', new BABYLON.Vector3(0, 1, -1), scene);
-
-		// This is where you create and manipulate meshes
-		const sphere = BABYLON.MeshBuilder.CreateSphere('sphere', {}, scene);
-
-		// Register a render loop to repeatedly render the scene
-		engine.runRenderLoop(function () {
-			scene.render();
-		});
-
-
-		// Watch for browser/canvas resize events
-		window.addEventListener('resize', function () {
-			engine.resize();
-		});
-	}
-}
-
-console.log('test');
 const canvas: HTMLCanvasElement = document.getElementById('renderCanvas') as HTMLCanvasElement;
-new Main(canvas);
+const suburbia = new CookieCutterSuburbia(canvas);
+
+const smartRender = new SmartRender(suburbia.scene);
+document.getElementById("smartRenderToggle").addEventListener("click", (event: Event) => {
+	smartRender.alwaysRender = !smartRender.alwaysRender;
+	(<HTMLElement>event.target).classList.toggle("toggleOff", !smartRender.alwaysRender);
+});
+
+// TODO: look into https://www.babylonjs.com/js/loader.js
+//suburbia.scene.debugLayer.show();
